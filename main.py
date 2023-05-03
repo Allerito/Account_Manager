2a2fd3cfa73a46ba72688ed40a83240ca0cb5ae7
Ruben Allera <Allera.Ruben.04@gmail.com>
2023-05-03 13:53:09 +0200

    Finished Account_Manager controls
    Added:
    - Now error will be check and the user will receive the error
    - Now the data will be control before it will be insert inside JSON file
    - Added docstring using pylint
    
    Bug:
    - Form doesn't run

--- main.py
+++ main.py
@@ -1,24 +1,80 @@
 import tkinter as tk
+from tkinter import messagebox
 import json
+import re
+
 __version__ = "0.1.0"
 
-def saveData():
+def read_data(temp_accounts):
+    """Read JSON data and upload data on temp_account
+    """
+    filename = "accounts.json"
+    try:
+        with open("accounts.json","r", encoding="utf-8") as a:
+            temp_accounts = json.load(a)
+    except FileNotFoundError:
+        with open(filename, "w", encoding="utf-8") as a:
+            a.write("[]")
+    except json.JSONDecodeError: 
+        with open(filename, "r", encoding="utf-8") as a:
+            if a.read() == "":
+                answer = tk.messagebox.askokcancel(title=None, message="Your file is empty. Are you sure to overwrite the file?", icon="warning")
+
+                if not answer:
+                    tk.messagebox.showinfo(title=None, message="Your file wasn't modified", icon="info")
+                    return
+
+def check_data():
+    """Check data that the user insert"""
     site = tb_site.get()
+    site_regex = re.compile(r"[A-Za-z]+\.[A-Za-z]+", re.IGNORECASE)
     username = tb_username.get()
     email = tb_email.get()
+    email_regex= re.compile(r"[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+", re.IGNORECASE)
     password = tb_password.get()
 
+    if len(site) > 0 and len(username) > 0 and len(email) > 0 and len(password) > 0:
+            if site_regex.match(site) and email_regex.match(email):
+                site = site.strip()
+                username = username.strip()
+                email = email.strip()
+                password = password.strip()
+                save_data(site, username, email, password, temp_accounts)
+            else:
+                tk.messagebox.showinfo(title=None, message="Your site or email doesn't conform to a normal one", icon="Error")
+    else:
+        tk.messagebox.showinfo(title=None, message="Your information aren't correct, check if you inser all the info", icon="Error")
+    
+
+def save_data(site, username, email, password, temp_accounts):
+    """Save data inside JSON file
+
+    :param site: Account site
+    :type site: string
+    :param username: Account username
+    :type username: string
+    :param email: Account email
+    :type email: string
+    :param password: Account password
+    :type password: string
+    :param temp_accounts: Temporal account manager
+    :type temp_accounts: list
+    """
     account_dict ={
         "site":site,
         "username":username,
         "email":email,
         "password":password
     }
-    json_object = json.dumps(account_dict, indent=4)
+    temp_accounts.append(account_dict)
+
+    json_object = json.dumps(temp_accounts, indent=4)
     with open("accounts.json", "w", encoding='utf-8') as outfile:
         outfile.write(json_object)
 
 if __name__=="__main__":
+    temp_accounts=[]
+
     win = tk.Tk()
     win.title(f"Account Manager {__version__}")
 
@@ -46,7 +102,7 @@ if __name__=="__main__":
     tb_password = tk.Entry(win)
     tb_password.pack()
 
-    button = tk.Button(win, text="Save data", command=saveData)
+    button = tk.Button(win, text="Save data", command=check_data)
     button.pack()
 
     win.mainloop()
