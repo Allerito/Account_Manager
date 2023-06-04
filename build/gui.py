40d2cd26469e488c106459c9ac82715e9cb3a60a
Allerito <allera.ruben.04@gmail.com>
2023-06-04 10:15:56 +0200

    Added new interface

---
+++ build/gui.py
@@ -0,0 +1,211 @@
+from pathlib import Path
+
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Aller\OneDrive\Documenti\GitHub\Account Manager\build\assets\frame0")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+window = Tk()
+
+window.geometry("751x327")
+window.configure(bg = "#FFFFFF")
+
+
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 327,
+    width = 751,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+canvas.create_text(
+    51.0,
+    86.0,
+    anchor="nw",
+    text="Username:",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+canvas.create_text(
+    51.0,
+    162.0,
+    anchor="nw",
+    text="Password:",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+canvas.create_text(
+    77.0,
+    124.0,
+    anchor="nw",
+    text="Email:",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+canvas.create_text(
+    88.0,
+    48.0,
+    anchor="nw",
+    text="Site:",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    216.0,
+    56.5,
+    image=entry_image_1
+)
+entry_1 = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=126.0,
+    y=45.0,
+    width=180.0,
+    height=21.0
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    216.0,
+    169.5,
+    image=entry_image_2
+)
+entry_2 = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_2.place(
+    x=126.0,
+    y=158.0,
+    width=180.0,
+    height=21.0
+)
+
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+entry_bg_3 = canvas.create_image(
+    216.0,
+    93.5,
+    image=entry_image_3
+)
+entry_3 = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_3.place(
+    x=126.0,
+    y=82.0,
+    width=180.0,
+    height=21.0
+)
+
+entry_image_4 = PhotoImage(
+    file=relative_to_assets("entry_4.png"))
+entry_bg_4 = canvas.create_image(
+    216.0,
+    131.5,
+    image=entry_image_4
+)
+entry_4 = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_4.place(
+    x=126.0,
+    y=120.0,
+    width=180.0,
+    height=21.0
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=17.0,
+    y=221.0,
+    width=293.0,
+    height=60.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=330.0,
+    y=146.0,
+    width=45.0,
+    height=46.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=617.0,
+    y=143.0,
+    width=76.0,
+    height=75.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=617.0,
+    y=45.0,
+    width=76.0,
+    height=75.0
+)
+window.resizable(False, False)
+window.mainloop()
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import tkinter as tk
-from tkinter import messagebox
 import json
 import re
 
+from .build import gui
+
+
 __version__ = "0.1.0"
 
-def read_data(temp_accounts: list)-> None:
-    """Read JSON data and upload data on temp_account
-    """
+def read_data()-> None:
+    """Read JSON data and upload data on temp_account"""
+    temp_accounts=[]
     filename = "accounts.json"
     try:
         with open("accounts.json","r", encoding="utf-8") as a:
@@ -23,15 +24,14 @@ def read_data(temp_accounts: list)-> None:
                 if not answer:
                     tk.messagebox.showinfo(title="Empty file", message="Your file wasn't modified", icon="info")
                     return
+    return temp_accounts
 
 def check_data()-> None:
     """Check data that the user insert"""
-    site = tb_site.get()
+    temp_accounts = read_data()
+    site, username, email, password = form_generator()
     site_regex = re.compile(r"[A-Za-z]+\.[A-Za-z]+", re.IGNORECASE)
-    username = tb_username.get()
-    email = tb_email.get()
-    email_regex= re.compile(r"[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+", re.IGNORECASE)
-    password = tb_password.get()
+    email_regex= re.compile(r"([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)@[A-Za-z]+\.[A-Za-z]+", re.IGNORECASE) #fix
 
     if len(site) > 0 and len(username) > 0 and len(email) > 0 and len(password) > 0:
         if site_regex.match(site) and email_regex.match(email):
@@ -61,7 +61,7 @@ def save_data(site: str, username: str, email: str, password: str, temp_accounts
     :param temp_accounts: Temporal account manager
     :type temp_accounts: list
     """
-    account_dict ={
+    account_dict = {
         "site":site,
         "username":username,
         "email":email,
@@ -79,38 +79,17 @@ def save_data(site: str, username: str, email: str, password: str, temp_accounts
     with open("accounts.json", "w", encoding='utf-8') as outfile:
         outfile.write(json_object)
 
+def dialog_manager():
+    back_button = tk.Button(win, text="back", command=step_calculator)
+    back_button.pack()
+
+    next_button = tk.Button(win, text="next", command=step_calculator)
+    next_button.pack()
+
+def step_calculator():
+    step_functions=[read_data, choose_filepath, form_generator, save_data]
+    if next_button == True
+
+
 if __name__=="__main__":
-    temp_accounts=[]
 
-    win = tk.Tk()
-    win.geometry("200x200")
-    win.title(f"Account Manager {__version__}")
-
-    lb_site = tk.Label(win, text="Site:")
-    lb_site.pack()
-
-    tb_site = tk.Entry(win)
-    tb_site.pack()
-
-    lb_username = tk.Label(win, text="Username:")
-    lb_username.pack()
-
-    tb_username = tk.Entry(win)
-    tb_username.pack()
-
-    lb_email = tk.Label(win, text="Email:")
-    lb_email.pack()
-
-    tb_email = tk.Entry(win)
-    tb_email.pack()
-
-    lb_password = tk.Label(win, text="Password:")
-    lb_password.pack()
-
-    tb_password = tk.Entry(win)
-    tb_password.pack()
-
-    button = tk.Button(win, text="Save data", command=check_data)
-    button.pack()
-
-    win.mainloop()
